3019ee4d70d466e2e0a7272c055a4948e997faeb
Alper Sakarya <alpersakarya@gmail.com>
2016-05-01 20:00:54 -0400

    fixing download url

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author=['Henry Lim', 'Alper Sakarya'],
   author_email='alpersakarya@gmail.com',
   url='https://github.com/AlperSakarya/earthview.git',
-  download_url='https://github.com/peterldowns/mypackage/tarball/0.1',
+  download_url='https://github.com/AlperSakarya/earthview/tarball/0.1',
   keywords=['desktop', 'wallpaper', 'earthview'],
   classifiers=[],
 )
