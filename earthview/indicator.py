be7d8044ad2bea67de450668b36cb5aa6e639e43
Alper Sakarya <alpersakarya@gmail.com>
2016-05-06 18:55:28 -0400

    Fixed logo and data.json so indicator.py can be runing from any directory

--- earthview/indicator.py
+++ earthview/indicator.py
@@ -1,21 +1,22 @@
 import signal
 import gi
+import json
+import os
+import urllib
 gi.require_version('Gtk', '3.0')
 gi.require_version('AppIndicator3', '0.1')
 gi.require_version('Notify', '0.7')
 from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
-import json
-import os
 from random import randint
-import urllib
 from gi.repository import Notify as notify
 
-APPINDICATOR_ID = 'myappindicator'
+APPINDICATOR_ID = 'EarthView'
 
 
 def main():
-    indicator = appindicator.Indicator.new(APPINDICATOR_ID, os.path.abspath('logo.png'), appindicator.IndicatorCategory.SYSTEM_SERVICES)
+    appLogo = os.path.dirname(os.path.realpath(__file__)) + "/logo.png"
+    indicator = appindicator.Indicator.new(APPINDICATOR_ID, os.path.abspath(appLogo), appindicator.IndicatorCategory.SYSTEM_SERVICES)
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
     indicator.set_menu(build_menu())
     notify.init(APPINDICATOR_ID)
@@ -39,7 +40,8 @@ def quit(source):
 
 
 def changewallpaper(source):
-    with open('data.json') as data_file:
+    urlFile = os.path.dirname(os.path.realpath(__file__)) + "/data.json"
+    with open(urlFile) as data_file:
         data = json.load(data_file)
 
     rand = randint(0, 1510)
