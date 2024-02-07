fb18bb31fe10386982da46d8fd4c41a3b5742b31
Alex Persian <alex_persian@me.com>
2024-02-07 14:46:09 -0500

    Replaces parser with multithreaded version from Sayan Goswami

--- script/parser.py
+++ script/parser.py
@@ -1,25 +1,43 @@
-import urllib2
-import json
+# Sayan Goswami (c) 6 July 2016
+import concurrent.futures
+import urllib.request
+from json import dumps
 from bs4 import BeautifulSoup
-result = []
-file = open('output.txt','a')
-for x in xrange(1000, 7030): #You should able to run this in parallel, but I don't know how to do it
-	x = str(x)
-	try:
-		print "Fetching" + x + " ..."
-		response = urllib2.urlopen('https://earthview.withgoogle.com/' + x)
-		html = response.read()
-		html = BeautifulSoup(html)
-		Region = str((html.find("div", class_="content__location__region")).text.encode('utf-8'))
-		Country = str((html.find("div", class_="content__location__country")).text.encode('utf-8'))
-		Everything = html.find("a", id="globe", href=True)
-		GMapsURL = Everything['href']
-		Image = 'https://www.gstatic.com/prettyearth/assets/full/' + x + '.jpg'
-		result.append({'region': Region, 'country': Country, 'map': GMapsURL, 'image': Image})
-	
-	except urllib2.HTTPError, e:
-		continue #If the page is 404, then it will skip to the next one
 
-meow = json.dumps(result) #because I love cat...
-file.write(meow)			
+result = []
+file = open('earthview.json','a')
+
+URLS = ['https://earthview.withgoogle.com/' + str(x) for x in range(1000,7030)]
+
+# Retrieve a single page and report the URL and contents
+def load_url(url, timeout):
+    with urllib.request.urlopen(url, timeout=timeout) as conn:
+        data =  conn.read()
+        html = BeautifulSoup(data,"html.parser")
+        Region = str((html.find("div", class_="content__location__region")).text)
+        Country = str((html.find("div", class_="content__location__country")).text)
+        Everything = html.find("a", id="globe", href=True)
+        GMapsURL = Everything['href']
+        Image = 'https://www.gstatic.com/prettyearth/assets/full/' + str(url.split('/')[-1]) + '.jpg'
+        a = {'region': Region, 'country': Country, 'map': GMapsURL, 'image': Image}
+        return a
+
+# We can use a with statement to ensure threads are cleaned up promptly
+with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    # Start the load operations and mark each future with its URL
+    future_to_url = {executor.submit(load_url, url, 60): url for url in URLS}
+    for future in concurrent.futures.as_completed(future_to_url):
+        url = future_to_url[future]
+        try:
+            res = future.result()
+        except Exception as exc:
+            pass
+        else:
+            # Do ya thing
+            result.append(res)
+            print("Fetched -> | " + str(url.split('/')[-1]) + " | ")
+
+
+final_file = dumps(result,indent=4) #Dump the json file finally
+file.write(final_file)
 file.close()
